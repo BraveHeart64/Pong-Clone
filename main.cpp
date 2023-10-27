4becdc4c135329d774fc4c0933279f18881e0780
Ryan <ryanlove64@gmail.com>
2023-10-27 10:45:02 -0500

    Progress on Collision
    If anybody has a better way of doing this, I am open to learning.  I had to adjust where I put to sprite on the screen. I learned how to retrieve the width and height of an image in Allegro5.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include"allegro5/allegro_font.h"
 #include"allegro5/allegro_ttf.h"
 #include"allegro5/keyboard.h"
+#include <allegro5/allegro_primitives.h>
 
 using namespace std;
 
@@ -30,15 +31,22 @@ class Sprite{
         }
 
           void Boundry(){
-            if(this->y >=402){ // keeps paddle from going to low off screen
+            if(this->y >=460){ // keeps paddle from going to low off screen
                 this->y = this->y -5;
+
             }
-            else if( this->y <= -1){ // keeps paddle form gooing to high
-            this->y = this->y +5;
+
+            if(this->y <= 35){
+                this->y = this->y +5;
+               // cout<< this->y;
 
             }
 
         }
+
+
+
+
 
         void Release(){
             this->up = false;
@@ -102,12 +110,18 @@ class Sprite{
 
 
 
-         Sprite(int x1, int y1, float width1, float height1){
-            image = 0;
+
+
+        // Sprite(int x1, int y1, float width1, float height1){
+        Sprite(string name, int x1, int y1){
+            cout<<"create";
+            image = al_load_bitmap(name.c_str());
             x = x1;
             y = y1;
-            width = width1;
-            height = height1;
+            width = al_get_bitmap_width(image)-3;//-3
+            height = al_get_bitmap_height(image)-50;//-50
+           // cout<< "Height: "<<height<<endl;
+          //  cout<< "width: " <<width;
             down = false;
             up = false;
             hit = false;
@@ -120,6 +134,7 @@ class Sprite{
                 if(image != 0){
                    al_destroy_bitmap(this->image);
                 }
+                cout<<"Destroyed ";
         }
 
 };
@@ -128,12 +143,15 @@ class Sprite{
 class Ball{
     private:
         bool hit;
-        int x =250;
-        int y =50;
+        int x = 250;
+        int y = 50;
+        int x_width, y_height;
         float height;
         float width;
 
         ALLEGRO_BITMAP* image = al_load_bitmap("Ball.png");
+
+
 
     public:
 
@@ -160,15 +178,31 @@ class Ball{
         }
 
         int GetHeight(){
-            return this->height;
+            return (this->height);
 
         }
+        int GetWidth(){
+            return this->width;
+        }
+
+        float GetWidth_x(){
+
+            return this->x_width;
+        }
+
+        float GetHeight_y(){
+
+            return this->y_height;
+
+        }
+
 
         int BallPath(){ // 250 start out
             if(hit == false){
 
 
              // x = x - 15;
+             x = x - 1;
             }else if(hit == true){
 
                // x =50;
@@ -191,6 +225,44 @@ class Ball{
 
 };
 
+
+class HitDetection{
+    private:
+        bool hit = false;
+
+    public:
+        void Collsion(Sprite *obj1, Ball obj2){
+
+            if( obj1->GetX() + obj1->GetWidth() > obj2.GetX() - obj2.GetWidth_x() &&
+                obj1->GetX() - obj1->GetWidth() < obj2.GetX() + obj2.GetWidth_x() &&
+                obj1->GetY()  + obj1->GetHeight()  > obj2.GetY() - obj2.GetHeight_y() &&
+                obj1->GetY() -  (obj1->GetHeight())  < obj2.GetY() + obj2.GetHeight_y()){
+
+                    cout<<"Hit";
+                    this->hit = true;
+                }
+                else{
+                    this->hit = false;
+                }
+//44
+            al_draw_filled_rectangle(obj1->GetX()  + obj1->GetWidth(),  obj1->GetY() + obj1->GetHeight() , obj1->GetX()  - obj1->GetWidth()+15, obj1->GetY() - obj1->GetHeight(), al_map_rgba_f(.7, 0, .6, .6));
+             al_draw_filled_rectangle(24+obj2.GetX()  - obj2.GetWidth(),  obj2.GetY() - obj2.GetHeight()+24 , (obj2.GetX())  + obj2.GetWidth(), obj2.GetY() + obj2.GetHeight(), al_map_rgba_f(.7, 0, .6, .6));
+         //   al_draw_filled_rectangle((obj1->GetX()+25)  - obj1->GetWidth_x(), ( obj1->GetY()+100 ) - (obj1->GetHeight_y()+25) , (obj1->GetX()-20)  + obj1->GetWidth_x(), (obj1->GetY()+8) + (obj1->GetHeight_y()-53 ), al_map_rgba_f(.7, 0, .6, .6));
+            //al_draw_filled_rectangle(obj1->GetX()  - obj1->GetWidth(), ( obj1->GetY()+153 ) - obj1->GetHeight() , obj1->GetX()  + obj1->GetWidth(), obj1->GetY() + (obj1->GetHeight()-53 ), al_map_rgba_f(.7, 0, .6, .6));
+
+        }
+
+
+
+
+
+        bool Contact(){
+
+            return hit;
+        }
+
+
+};
 
 /* I need to figure out how to make this code work for hit detection
  bool Overlap(float spone_min, float spone_max,float sptwo_min, float sptwo_max){
@@ -228,8 +300,8 @@ class Ball{
 
 int main(int argc, char* argv[]){
 
-
-
+string name = "Paddle.png";
+    HitDetection col;
 
 
     al_init();
@@ -239,10 +311,12 @@ int main(int argc, char* argv[]){
     al_install_keyboard();
     al_install_mouse();
     al_install_joystick();
+    al_init_primitives_addon();
 
 
-    Sprite* p_one = new  Sprite(3,50,19,101);
-    Sprite* p_two = new  Sprite(478,50,19,101);
+   // Sprite* p_one = new  Sprite(name,3,50);
+    Sprite* p_one = new  Sprite(name,3,150);
+    Sprite* p_two = new  Sprite(name,478,150);
 
     Ball ball;
 
@@ -346,14 +420,16 @@ int main(int argc, char* argv[]){
             p_one->MovePlayer();
             p_one->Boundry();
             //al_draw_bitmap(player_one,10,50,1);
-            al_draw_bitmap(player_one,p_one->GetX(),p_one->GetY(),1);
-            al_draw_bitmap(player_two,p_two->GetX(),p_two->GetY(),1);
+            al_draw_bitmap(player_one,p_one->GetX(),p_one->GetY()-50,1);
+            al_draw_bitmap(player_two,p_two->GetX()-5,p_two->GetY()-50,1);
             al_draw_bitmap(ball.GetImage(),ball.BallPath(),50,1);//250
 
             //p_one.SetHit(p_one.Collide(p_one, ball));
            // p_two.SetHit(p_two.Collide(p_two, ball));
            //  ball.SetHit(p_one.Collide(p_one, ball));
 
+            col.Collsion(p_one,ball);
+            col.Collsion(p_two,ball);
 
 
             al_flip_display();
